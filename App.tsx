10cb1a2861a06fddedc9ba692181a8d8bbdeb650
ricardocardoso90 <ricardomanolo90@gmail.com>
2024-10-11 15:55:05 -0300

    update commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Map, { Callout, Marker } from 'react-native-maps';
 
 export default function App() {
+  const coordinate = {
+    latitude: -23.561247393962343,
+    longitude: -46.656661925026796,
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Map
+        style={StyleSheet.absoluteFill}
+        initialRegion={{
+          latitude: coordinate.latitude,
+          longitude: coordinate.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      >
+        <Marker coordinate={coordinate}>
+          <Callout style={styles.callout}>
+            <View>
+              <Text style={styles.title}>Loja do Ricardo</Text>
+              <Text style={styles.address}>Av. Paulista, SP</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </Map>
     </View>
   );
 }
@@ -13,8 +34,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  callout: {
+    padding: 12,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  address: {
+    fontSize: 14,
+  }
 });
